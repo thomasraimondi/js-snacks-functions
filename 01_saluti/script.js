9a17d45e87204bbc7acbdf657d733f8a14e74c01
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-08 14:52:34 +0200

    Initial Repo

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,28 +5,28 @@ const userName = "Mario";
 // Dichiara la funzione qui.
 
 //Funzione tradizionale
-/**
- * Funzione che dato un nome restituisce un saluto
- * @param {string} stringName User name
- * @returns {string} welcomeString - stringa di saluto
- */
-function welcomeTraditional(stringName) {
-  const welcomeString = "Ciao " + stringName;
-  return welcomeString;
-}
+// /**
+//  * Funzione che dato un nome restituisce un saluto
+//  * @param {string} stringName User name
+//  * @returns {string} welcomeString - stringa di saluto
+//  */
+// function welcomeTraditional(stringName) {
+//   const welcomeString = "Ciao " + stringName;
+//   return welcomeString;
+// }
 
-//Arrow Function
-const welcomeArrow = (stringName) => {
-  return (welcomeString = "Ciao " + stringName);
-};
+// //Arrow Function
+// const welcomeArrow = (stringName) => {
+//   return (welcomeString = "Ciao " + stringName);
+// };
 
-//Arrow Function Short
-const welcomeArrowShort = (stringName) =>
-  (welcomeString = "Ciao " + stringName);
+// //Arrow Function Short
+// const welcomeArrowShort = (stringName) =>
+//   (welcomeString = "Ciao " + stringName);
 
-// Invoca la funzione qui e stampa il risultato in console
-console.log(welcomeTraditional(userName));
-console.log(welcomeArrow(userName));
-console.log(welcomeArrowShort(userName));
+// // Invoca la funzione qui e stampa il risultato in console
+// console.log(welcomeTraditional(userName));
+// console.log(welcomeArrow(userName));
+// console.log(welcomeArrowShort(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
