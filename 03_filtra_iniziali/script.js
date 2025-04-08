1b2a2375aad5ac12f19166242291d162b11d7c5b
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-08 15:26:45 +0200

    Complete 03 Filtra Iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,34 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+/**
+ * Funzione che mi restituisce sempre la prima lettere
+ * @param {string} name stringa in input
+ * @returns {string} name[0]
+ */
+function firstCharTradition(name) {
+  return name[0];
+}
+/**
+ * Funzione che dato un array e un carattere serca le parole che iniziano per quel parattere
+ * @param {array} stringNames elenco di stringhe
+ * @param {string} findChar carattere da confrontare
+ * @returns {array} stringFound parole che iniziano con il carattere indicato.
+ */
+function search(stringNames, findChar) {
+  const stringFound = [];
+  for (i = 0; i < stringNames.length; i++) {
+    const currentValue = stringNames[i];
 
+    if (firstCharTradition(stringNames[i]) === findChar) {
+      stringFound.push(currentValue);
+    }
+  }
+  return stringFound;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(search(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
