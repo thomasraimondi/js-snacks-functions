4ec43a4ae59c10e0c60cb8f8d24b0776dba5edb7
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-08 16:45:52 +0200

    Complete 05 Bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,34 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Thomas";
+const date = new Date();
+console.log(date);
 
 // Dichiara la funzione qui.
-
+/**
+ * Funzione che dato un nome restituisce un saluto
+ * @param {string} stringName User name
+ * @returns {string} welcomeString - stringa di saluto
+ */
+function welcomeTraditional(stringName) {
+  const welcomeString = timeOfDay(17) + stringName;
+  return welcomeString;
+}
+/**
+ * Funzione che ritorna il momento della giornata
+ * @param {number} hour
+ * @returns welcomeMessagge
+ */
+function timeOfDay(hour) {
+  let welcomeMessagge;
+  if (hour <= 13) welcomeMessagge = "Buongiorno ";
+  else if (hour > 13 && hour <= 17) welcomeMessagge = "Buon Pomeriggio ";
+  else welcomeMessagge = "Buona sera ";
+  return welcomeMessagge;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(welcomeTraditional(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
