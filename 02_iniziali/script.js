7c22fb4c413bc3646f4ed87111a8a78d00484e41
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-08 15:06:16 +0200

    Complete 02 Iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,31 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const initials = [];
 
 // Dichiara la funzione qui.
 
+//Funzione tradizionale
+/**
+ * Funzione che mi restituisce la prima lettera di una stringa
+ * @param {string} name
+ */
+function firstCharTradition(name) {
+  return name[0];
+}
+
+//Arrow Funtion
+const firstCharArrow = (name) => {
+  return name[0];
+};
+
+//Arrow function Short
+const firstCharArrowShort = (name) => name[0];
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+for (i = 0; i < names.length; i++) {
+  const currentValue = firstCharTradition(names[i]);
+  initials.push(currentValue);
+}
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(initials);
